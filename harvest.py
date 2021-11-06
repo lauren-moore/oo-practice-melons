b5aaf08197e99e1b5bd41b858c2000ef7d43950e
Lauren <laurencaroleen@gmail.com>
2021-11-06 19:49:03 +0000

    Completed Part 1 and class Melon in Part 2

--- harvest.py
+++ harvest.py
@@ -25,7 +25,7 @@ class MelonType:
         """Add a food pairing to the instance's pairings list."""
 
         # Fill in the rest
-        self.pairings.extend(pairing)
+        self.pairings.append(pairing)
         
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
@@ -42,29 +42,46 @@ def make_melon_types():
     # Fill in the rest
     musk = MelonType("musk", "Muskmelon", 1998, "green", True, True)
     musk.add_pairing("mint")
+    all_melon_types.append(musk)
     cas = MelonType("cas", "Casaba", 2003, "orange", True, False)
-    cas.add_pairing(["strawberrys", "mint"])
+    all_melon_types.append(cas)
+    cas.add_pairing("strawberrys")
+    cas.add_pairing("mint")
     cren = MelonType("cren", "Crenshaw", 1996, "green", True, False)  
     cren.add_pairing("prosciutto")  
+    all_melon_types.append(cren)
     yw = MelonType("yw", "Yellow Watermelon", 2013, "yellow", True, True)
     yw.add_pairing("ice cream")
+    all_melon_types.append(yw)
     
-    all_melon_types.append(musk, )
 
     return all_melon_types
 
+make_melon_types()
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
     # Fill in the rest
-    
+    for melon in melon_types:
+        print(f"{melon.name} pairs with")
+        for pairing in melon.pairings:
+            print(f"- {pairing}")
+
+print_pairing_info(make_melon_types())
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
-    # Fill in the rest
+    melon_dict = {}
 
+    for melon in melon_types:
+        melon_dict[melon.code] = melon.name
+    
+    print(melon_dict)
+    return melon_dict
+
+make_melon_type_lookup(make_melon_types())
 
 ############
 # Part 2   #
@@ -76,12 +93,30 @@ class Melon:
 
     # Fill in the rest
     # Needs __init__ and is_sellable methods
+    def __init__(self, code, shape, color, field_num, harvested_by):
+
+        self.code = code
+        self.shape = shape
+        self.color = color
+        self.field_num = field_num
+        self.harvested_by = harvested_by
+
+
+    def is_sellable(self):
+        if self.shape > 5 and self.color > 5 and self.field_num != 3:
+            return True
+
+
+
+
+
 
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
     # Fill in the rest
+    Melon_1 = Melon("yw", 8, 7, 2, "Sheila")
 
 
 def get_sellability_report(melons):
